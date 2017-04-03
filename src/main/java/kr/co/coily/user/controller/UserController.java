6f2ea0ab63b65d108116dd66675620d49cfcdc81
DESKTOP-7SE89SC\hac <hac0102@naver.com>
2017-04-03 10:48:09 +0900

    sprin-context-support 추가(pom.xml) , 회원가입 - 이메일 인증

--- src/main/java/kr/co/coily/user/controller/UserController.java
+++ src/main/java/kr/co/coily/user/controller/UserController.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
